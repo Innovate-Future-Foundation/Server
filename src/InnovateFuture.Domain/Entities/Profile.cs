3eb1007a95a2dd320291bb9d19524e44f8e0dcdb
Leo Zhang <leo.zhang.ele@gmail.com>
2025-01-09 01:33:50 +1100

    revise the profile, move the navigation property to the corresponding id field

--- src/InnovateFuture.Domain/Entities/Profile.cs
+++ src/InnovateFuture.Domain/Entities/Profile.cs
@@ -4,23 +4,22 @@ public class Profile
 {
     public Guid ProfileId { get; private set; }
     public Guid UserId { get; private set; }
+    public User User { get; private set; }
     public Guid? OrgId { get; private set; }
-    public Guid RoleId { get; private set; }
+    public Organisation? Organisation { get; private set; }
+    public Guid RoleId { get; private set; }   
+    public Role Role { get; private set; }
     public Boolean IsActive { get; private set; }
     public string? Email { get; private set; }
     public Guid? InvitedBy { get; private set; }
+    public Profile? InvitedByProfile { get; private set; }
     public Guid? SupervisedBy { get; private set; }
+    public Profile? SupervisedByProfile { get; private set; }
     public string? Name { get; private set; }
     public string? Phone { get; private set; }
     public string? Avatar { get; private set; }
-    
-    // navigation properties
-    public User User { get; private set; }
-    public Role Role { get; private set; }
-    public Organisation? Organisation { get; private set; }
-    public Profile? InvitedByProfile { get; private set; }
-    public Profile? SupervisedByProfile { get; private set; }
-    public DateTime CreatedAt { get; private set; }
+   
+        public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
     public Profile() { } 
     public Profile(
