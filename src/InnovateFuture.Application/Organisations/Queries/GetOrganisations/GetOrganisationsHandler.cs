e1a224b5a03e7bc914418135b9e73732beff620a
Raymond <zhuyuleiraymond@outlook.com>
2025-01-10 22:14:16 +1100

    refactor: changes on Orgnisation curd functions

--- src/InnovateFuture.Application/Organisations/Queries/GetOrganisations/GetOrganisationsHandler.cs
+++ src/InnovateFuture.Application/Organisations/Queries/GetOrganisations/GetOrganisationsHandler.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using MediatR;
 using InnovateFuture.Domain.Entities;
 using InnovateFuture.Infrastructure.Organisations.Persistence.Interfaces;
@@ -13,10 +14,25 @@ public class GetOrganisationsHandler : IRequestHandler<GetOrganisationsQuery, Li
         _orgRepository = orgRepository;
     }
 
-    public async Task<List<Organisation>> Handle(GetOrganisationsQuery request, CancellationToken cancellationToken)
+    public async Task<List<Organisation>> Handle(GetOrganisationsQuery query, CancellationToken cancellationToken)
     {
-        var organisations = new List<Organisation>();
-        await _orgRepository.GetAllAsync(organisations);
-        return organisations;
+        Expression<Func<Organisation, bool>> queryPredicate;
+        var queriesEmpty = query.GetType().GetProperties().All(p=>p.GetValue(query)==null);
+        if (queriesEmpty)
+        {
+            queryPredicate = o => true; // Fetch all users
+        }
+        else
+        {
+            // Build predicate based on query conditions
+            queryPredicate = o =>
+                (string.IsNullOrEmpty(query.Email) || o.Email == query.Email) &&
+                (string.IsNullOrEmpty(query.OrgName) || o.OrgName == query.OrgName) &&
+                (!query.Status.HasValue || o.Status == query.Status);
+        }
+
+        var organisations = await _orgRepository.GetAnyAsync(queryPredicate);
+        
+        return organisations.ToList();
     }
 }
--- src/InnovateFuture.Infrastructure/Organisations/Persistence/Interfaces/IOrgRepository.cs
+++ src/InnovateFuture.Infrastructure/Organisations/Persistence/Interfaces/IOrgRepository.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using InnovateFuture.Domain.Entities;
 
 namespace InnovateFuture.Infrastructure.Organisations.Persistence.Interfaces;
@@ -7,5 +8,5 @@ public interface IOrgRepository
     Task<Organisation> GetByIdAsync(Guid id);
     Task AddAsync(Organisation organisation);
     Task UpdateAsync();
-    Task<IEnumerable<Organisation>>GetAnyAsync(Expression<Func<Organisation, bool>> predicate);
+    Task<IEnumerable<Organisation>> GetAnyAsync(Expression<Func<Organisation, bool>> predicate);
 }
