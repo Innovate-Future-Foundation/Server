ae77a51404901cfb131444d51b2054bbd1ff2099
Raymond Zhu <147613036+YuleiZhu-Raymond@users.noreply.github.com>
2025-01-10 21:40:52 +1100

    Update src/InnovateFuture.Infrastructure/Organisations/Persistence/Interfaces/IOrgRepository.cs
    Co-authored-by: Chelsea Yang <100550092+qingyanyang@users.noreply.github.com>

--- src/InnovateFuture.Infrastructure/Organisations/Persistence/Interfaces/IOrgRepository.cs
+++ src/InnovateFuture.Infrastructure/Organisations/Persistence/Interfaces/IOrgRepository.cs
@@ -5,7 +5,7 @@ namespace InnovateFuture.Infrastructure.Organisations.Persistence.Interfaces;
 public interface IOrgRepository
 {
     Task<Organisation> GetByIdAsync(Guid id);
-    Task AddAsync(Organisation order);
+    Task AddAsync(Organisation organisation);
     Task UpdateAsync();
     Task GetAllAsync(List<Organisation> organisations);
 }
