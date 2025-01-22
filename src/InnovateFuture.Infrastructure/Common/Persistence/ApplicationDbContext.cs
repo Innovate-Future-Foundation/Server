3169901462118638ef792df5d55e7c4a990c1e09
fanwangcareer <127468735+fanwangcareer@users.noreply.github.com>
2025-01-22 13:06:49 +1100

    update migration condition

--- src/InnovateFuture.Infrastructure/Common/Persistence/ApplicationDbContext.cs
+++ src/InnovateFuture.Infrastructure/Common/Persistence/ApplicationDbContext.cs
@@ -4,10 +4,8 @@ using Microsoft.EntityFrameworkCore;
 namespace InnovateFuture.Infrastructure.Common.Persistence;
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Role> Roles { get; set; }
-    public DbSet<Profile> Profiles { get; set; }
-    public DbSet<Organisation> Organisations { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
