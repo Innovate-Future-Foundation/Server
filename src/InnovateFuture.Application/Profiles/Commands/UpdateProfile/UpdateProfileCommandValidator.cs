7109657e47fb68f24f48866d1268604bcd4f0a69
Chelsea Yang <100550092+qingyanyang@users.noreply.github.com>
2025-02-04 17:59:31 +1030

    fix: switch name and email position when pass parameters (#39)

--- src/InnovateFuture.Application/Profiles/Commands/UpdateProfile/UpdateProfileCommandValidator.cs
+++ src/InnovateFuture.Application/Profiles/Commands/UpdateProfile/UpdateProfileCommandValidator.cs
@@ -7,14 +7,30 @@ public class UpdateProfileCommandValidator : AbstractValidator<UpdateProfileComm
     {
         RuleFor(x => x.ProfileId)
             .NotEmpty().WithMessage("Profile Id is required.");
-        RuleFor(x => x.Email)
-            .MaximumLength(255).WithMessage("Email must not exceed 255 characters.")
-            .EmailAddress().WithMessage("Kindly enter a valid Email Address.");
-        RuleFor(x => x.Name)
-            .MaximumLength(255).WithMessage("Name must not exceed 255 characters.");
-        RuleFor(x => x.Phone)
-            .Matches("^\\+61\\s4\\d{8}$").WithMessage("Kindly enter a valid AU Phone Number.");
-        RuleFor(x => x.AvatarUrl)
-            .MaximumLength(500).WithMessage("Avatar must not exceed 500 characters.");
+        
+        When(x => !string.IsNullOrEmpty(x.Email), () =>
+        {
+            RuleFor(x => x.Email)
+                .MaximumLength(255).WithMessage("Email must not exceed 255 characters.")
+                .EmailAddress().WithMessage("Kindly enter a valid Email Address.");
+        });
+        
+        When(x => !string.IsNullOrEmpty(x.Name), () =>
+        {
+            RuleFor(x => x.Name)
+                .MaximumLength(255).WithMessage("Name must not exceed 255 characters.");
+        });
+        
+        When(x => !string.IsNullOrEmpty(x.Phone), () =>
+        {
+            RuleFor(x => x.Phone)
+                .Matches("^\\+61\\s4\\d{8}$").WithMessage("Kindly enter a valid AU Phone Number.");
+        });
+        
+        When(x => !string.IsNullOrEmpty(x.AvatarUrl), () =>
+        {
+            RuleFor(x => x.AvatarUrl)
+                .MaximumLength(500).WithMessage("Avatar url must not exceed 500 characters.");
+        });
     }
 }
