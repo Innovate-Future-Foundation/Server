740f35776722ff49e4b76483cf03177b4e6c49de
Chelsea.Y <yangqingyan0@gmail.com>
2025-02-07 18:17:02 +1030

    fix: readd missing commit

--- src/InnovateFuture.Application/Profiles/Queries/GetProfiles/GetProfilesHandler.cs
+++ src/InnovateFuture.Application/Profiles/Queries/GetProfiles/GetProfilesHandler.cs
@@ -39,6 +39,7 @@ public class GetProfilesHandler : IRequestHandler<GetProfilesQuery, (List<Profil
                 predicate = predicate.And(p =>
                     (filters.OrgId==null || p.OrgId==filters.OrgId) &&
                     (validGuids.Count==0 || validGuids.Contains(p.RoleId)) &&
+                    (filters.Supervisor == null|| p.Supervisor == filters.Supervisor) &&
                     (!filters.IsConfirmed.HasValue || p.IsConfirmed == filters.IsConfirmed) &&
                     (!filters.IsActive.HasValue || p.IsActive == filters.IsActive));
             }
